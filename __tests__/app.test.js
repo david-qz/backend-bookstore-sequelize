33966391f7bc1e34e58795eaf720f56d68f1a616
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-17 15:14:39 -0700

    Repository set up

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,13 @@
 // const app = require('../lib/app');
 const db = require('../lib/models');
 describe('backend-express-template routes', () => {
-  beforeEach(async () => {
-    await db.sequelize.sync({ force: true });
-  });
-  afterAll(async () => {
-    await db.sequelize.close();
-  });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+    beforeEach(async () => {
+        await db.sequelize.sync({ force: true });
+    });
+    afterAll(async () => {
+        await db.sequelize.close();
+    });
+    it('example test - delete me!', () => {
+        expect(1).toEqual(1);
+    });
 });
