c918b0a46098d8438e9bf9d812ab4380c5d1e22b
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-08-17 15:14:39 -0700

    Repository set up

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const API_URL = process.env.API_URL || 'http://localhost';
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`🚀  Server started on ${API_URL}:${PORT}`);
+    // eslint-disable-next-line no-console
+    console.log(`🚀  Server started on ${API_URL}:${PORT}`);
 });
 
 process.on('exit', () => {
-  // eslint-disable-next-line no-console
-  console.log('👋  Goodbye!');
+    // eslint-disable-next-line no-console
+    console.log('👋  Goodbye!');
 });
